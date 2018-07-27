3e6a917c0a91895fca744e82478576e161920afd
tolisich <it14126@uom.edu.gr>
2018-07-28 00:56:08 +0300

    Complete Functionality
    All the functionality of the project has been implemented.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
-import './App.css';
 import Map from './Map';
-//import BurgerMenu from './BurgerMenu';
 import { slide as Menu } from 'react-burger-menu'
 import escapeRegExp from 'escape-string-regexp'
+import './App.css';
 
 class App extends Component {
   state = {
     query: '',
+    clickedMarker: null,
     markers: [
       {
+        id: '4e2836ff18384dd0a0ddb777',
         lat: 40.6323592,
         lng: 22.9452542,
         title: 'Roman Forum Thessaloniki'
       },
       {
+        id: '4e7c83db3151cef2f53f7642',
         lat: 40.636792,
         lng: 22.943661,
         title: 'Church of Panagia Chalkeon'
       },
       {
+        id: '4bd0aa27b221c9b60728d4d0',
         lat: 40.633165,
         lng: 22.941090,
         title: 'Electra Palace Hotel'
       },
       {
+        id: '4bd9a4d1d2cbc928b330d1ad',
         lat: 40.626855,
         lng: 22.948451,
         title: 'White Tower'
       },
       {
+        id: '4fc4c7dfe4b0a6258ec4eec8',
         lat: 40.630886,
         lng: 22.959233,
         title: 'Aristotle University'
@@ -58,6 +63,10 @@ class App extends Component {
     this.setState({ query: query.trim() })
   )
 
+  setClickedMarker = (marker) => (
+    this.setState({ clickedMarker: marker })
+  )
+
   render() {
     let showingMarkers
     if(this.state.query) {
@@ -77,15 +86,16 @@ class App extends Component {
             placeholder="Filter Markers..." 
             value={this.state.query} 
             onChange={(event) => (this.updateQuery(event.target.value))}/>
+
           <ul>
             {showingMarkers.map(marker => (
-              <li key={marker.title}>{marker.title}</li>
+              <li key={marker.id} onClick={(event) => this.setClickedMarker(marker)}>{marker.title}</li>
             ))}
           </ul>
         </Menu>
         </div>
         
-        <Map markers={showingMarkers}/>
+        <Map markers={showingMarkers} clickedMarker={this.state.clickedMarker}/>
       </div>
       
     );
--- src/Map.js
+++ src/Map.js
@@ -17,6 +17,12 @@ class Map extends Component {
     if(prevProps.markers !== this.props.markers) {
       this.renderMarkers(this.state.map, this.state.maps, this.props.markers)
     }
+
+    if(prevProps.clickedMarker !== this.props.clickedMarker) {
+      let stateMarker = this.state.markers.filter(marker => marker.title === this.props.clickedMarker.title)
+
+      this.state.maps.event.trigger(stateMarker[0], 'click')
+    }
   }
 
   renderMarkers (map, maps, markers) {
@@ -26,7 +32,7 @@ class Map extends Component {
     }
 
     this.state.markers.forEach(marker => marker.setMap(null))
-    
+
     let showingMarkers = []
     markers.map(marker => {
       let latlng = {
@@ -38,6 +44,35 @@ class Map extends Component {
         map,
         title: marker.title
       });
+      
+      let infowindow = new maps.InfoWindow();
+
+      newMarker.addListener('click', function() {
+        infowindow.setContent("")
+        
+        if (newMarker.getAnimation() != null) {
+          newMarker.setAnimation(null);
+        } else {
+          newMarker.setAnimation(maps.Animation.BOUNCE);
+          setTimeout(function(){ newMarker.setAnimation(null); }, 1350);
+        }
+
+        fetch('https://api.foursquare.com/v2/venues/' + marker.id + '?&oauth_token=HKX2L43D2IXFD0T1NTUVZRZZGU5M3COJXIHXKE5N5IW0E0YI&v=20180727')
+        .then(function(response) {
+          return response.json()
+        })
+        .then(function(data) {
+          let reviews = data.response.venue.tips.groups.filter(group => group.type === 'others' && group.name === 'All tips')
+          
+          let reviewItemCnt = Math.floor(Math.random()*(5+1))
+          infowindow.setContent("<b>" + newMarker.title + "</b><br />" + reviews[0].items[reviewItemCnt].text)
+        })
+        .catch(function(error) {
+          infowindow.setContent("Data couldn't be loaded")
+        })
+
+        infowindow.open(map, newMarker)
+      })
 
       showingMarkers.push(newMarker)
     })
@@ -48,7 +83,8 @@ class Map extends Component {
   render() {
     return (
       <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
+        <GoogleMapReact 
+          yesIWantToUseGoogleMapApiInternals={true}
           bootstrapURLKeys={{ key: 'AIzaSyBrFTM2440iu_37qfqdya2x8GTX4J4b_GM' }}
           defaultCenter={this.state.center}
           defaultZoom={this.state.zoom}
